dbb23213fa72ee9c9f1ca4aa5347260349695d61
Sigma Student <yadavnirmal725@gmail.com>
2025-02-03 01:42:28 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,7 +1,7 @@
-const cloudinary = require("cloudinary").v2.uploader;
+const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-cloudinary.config.uploader({
+cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
